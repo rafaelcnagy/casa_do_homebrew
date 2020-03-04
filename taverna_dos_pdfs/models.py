e0f1ffb83161b4b5342124fba78410c40f843f15
rafaelcnagy <rafaelcnagy@hotmail.com>
2020-03-04 00:35:41 -0300

    Cria model dos arquivos PDFs

--- taverna_dos_pdfs/models.py
+++ taverna_dos_pdfs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class PdfFile(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    path = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    votes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
