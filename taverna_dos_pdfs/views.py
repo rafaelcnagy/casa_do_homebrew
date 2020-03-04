adb76dc0f8d34e403010e79230a74abce90a0063
rafaelcnagy <rafaelcnagy@hotmail.com>
2020-03-04 00:36:15 -0300

    Adiciona página inicial simples

--- taverna_dos_pdfs/views.py
+++ taverna_dos_pdfs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from taverna_dos_pdfs.models import PdfFile
+
+
+def pdf_list(request):
+    pdfs = PdfFile.objects.filter(deleted_at__isnull=False).order_by('votes')
+    return render(request, 'taverna_dos_pdfs/pdf_list.html', {'pdfs': pdfs})
