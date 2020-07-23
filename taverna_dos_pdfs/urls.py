37a5bbb9b658767a0c6a92d47c43920a456216a2
rafaelcnagy <rafaelcnagy@hotmail.com>
2020-07-23 12:23:25 -0300

    Utiliza django.contrib.auth e add password reset

--- taverna_dos_pdfs/urls.py
+++ taverna_dos_pdfs/urls.py
@@ -1,13 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.pdf_list, name='pdf_list'),
     path('create', views.create_pdf, name='create_pdf'),
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
     path('register', views.register, name='register'),
 ]
 
--- taverna_dos_pdfs/views.py
+++ taverna_dos_pdfs/views.py
@@ -31,24 +31,6 @@ def handle_uploaded_file(file):
             destination.write(chunk)
 
 
-def login_view(request):
-    if request.user.is_authenticated:
-        return HttpResponseRedirect('/')
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect('/')
-        else:
-            messages.error(request, 'Login incorreto ')
-
-    else:
-        return render(request, 'registration/login.html', {})
-
-
 def register(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect('/')
@@ -65,8 +47,3 @@ def register(request):
     else:
         form = RegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
-
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect('/')
