cf8c16cd64e01396e59fdea963e571f3f5ba49a2
rafaelcnagy <rafaelcnagy@hotmail.com>
2020-07-24 20:26:42 -0300

    Adiciona endereço emails de debug

--- casa_do_homebrew/settings.py
+++ casa_do_homebrew/settings.py
@@ -126,5 +126,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
