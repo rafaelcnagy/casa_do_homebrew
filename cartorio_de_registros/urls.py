1db38eadbf2994742ffe6e98de595d28ad8e9508
rafaelcnagy <rafaelcnagy@hotmail.com>
2020-08-01 16:15:34 -0300

    Correção view

--- cartorio_de_registros/urls.py
+++ cartorio_de_registros/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(template_name='cartorio_de_registros/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset',
-         auth_views.PasswordResetDoneView.as_view(template_name='cartorio_de_registros/password_reset_form.html'),
+         auth_views.PasswordResetView.as_view(template_name='cartorio_de_registros/password_reset_form.html'),
          name='password_reset'),
     path('password_reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name='cartorio_de_registros/password_reset_done.html'),
